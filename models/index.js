c9d70b1d756d07c8732a36af6f488efc7d9b24f4
Linconner <linconsegato32@gmail.com>
2025-04-03 15:07:05 -0300

    Adicionando .gitignore e ignorando node_modules

---
+++ models/index.js
@@ -0,0 +1,18 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const config = require("../config/config.json");
+
+const sequelize = new Sequelize(config.development);
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.Categoria = require("./categoria")(sequelize, DataTypes);
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+module.exports = db;
