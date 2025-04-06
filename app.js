30d4b8ef0f681f8476559a6e2f5bcaac73430824
Lincon <linconsegato32@gmail.com>
2025-04-06 19:11:03 -0300

    tentando colocar no vercel

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ const path = require("path");
 const app = express();
 const db = require("./models");
 
+// Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Serve arquivos estáticos como CSS e imagens
+// Arquivos estáticos (CSS, imagens, etc)
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Configuração da view engine EJS
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// Rotas
 const indexRouter = require("./routes/index");
 const professorRouter = require("./routes/professores");
 const alunoRouter = require("./routes/alunos");
@@ -20,19 +23,8 @@ app.use("/", indexRouter);
 app.use("/professores", professorRouter);
 app.use("/alunos", alunoRouter);
 
-db.sequelize.authenticate()
-  .then(() => {
-    console.log('Conexão com o banco estabelecida');
-    return db.sequelize.sync();
-  })
-  .then(() => {
-    console.log('Modelos sincronizados');
-    app.listen(3000, () => {
-      console.log("Servidor em execução na porta 3000");
-    });
-  })
-  .catch(err => {
-    console.error('Erro ao conectar com o banco:', err);
-    process.exit(1);
-  });
+// Sincroniza com o banco de dados
+db.sequelize.sync();
 
+// Exporta o app para Vercel usar
+module.exports = app;
